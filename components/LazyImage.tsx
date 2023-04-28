a4b0b8c4b855341ccc6abcf69e003f5f66a658e5
John Pablo <john.davis.2015.03@gmail.com>
2023-04-28 12:10:22 -0500

    add lodash

--- components/LazyImage.tsx
+++ components/LazyImage.tsx
@@ -1,36 +1,60 @@
 import { ImgHTMLAttributes, useEffect, useRef, useState } from "react";
 
-type LazyImageProps = {
+// type LazyImageProps = {
+//   src: string;
+//   alt: string;
+// };
+// type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
+// type Props = LazyImageProps & ImageNative;
+interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   src: string;
   alt: string;
-};
-type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
-type Props = LazyImageProps & ImageNative;
+  onLazyLoad?: (img: HTMLImageElement) => void;
+}
 
-export const LazyImage = ({ src, alt, ...imgProps }: Props): JSX.Element => {
+export const LazyImage = ({
+  src,
+  alt,
+  onLazyLoad,
+  ...imgProps
+}: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
+  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
   const [currentSrc, setCurrentSrc] = useState(
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
   );
 
   useEffect(() => {
+    if (isLazyLoaded) {
+      return;
+    }
     // nuevo observador
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         // onIntersection -> console.log
-        if (entry.isIntersecting) {
+        if (!entry.isIntersecting || !node.current) {
+          return;
           // console.log("Hey you!");
-          setCurrentSrc(src);
+        }
+
+        setCurrentSrc(src);
+        observer.disconnect();
+        setIsLazyLoaded(true);
+
+        if (typeof onLazyLoad === "function") {
+          onLazyLoad(node.current);
         }
       });
     });
+
     // observe node
     if (node.current) {
       observer.observe(node.current);
     }
+
     // desconectar
     return () => observer.disconnect();
-  }, [src]);
+  }, [src, onLazyLoad, isLazyLoaded]);
 
   return <img ref={node} src={currentSrc} {...imgProps} />;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,57 @@
-import { LazyImage } from "@/components/LazyImage";
-import { MouseEventHandler } from "react";
 import { useState } from "react";
+import { MouseEventHandler } from "react";
+import { LazyImage } from "@/components/LazyImage";
+import { random } from "lodash";
 
 // Generate a random function between 1 and 123 with const?
-const random = (): number => Math.floor(Math.random() * 123) + 1;
+// const random = (): number => Math.floor(Math.random() * 123) + 1;
+const MyRandom = () => random(1, 123);
 
 // generate simple unique id?
 const generateId = () => Math.random().toString(36).substring(2, 9);
 
-type ImageItem = {
-  id: string;
-  url: string;
-};
+// Se hizo un tipo global y esta en el archivo app.d.ts
+// type ImageItem = {
+//   id: string;
+//   url: string;
+// };
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageItem>>([]);
+  const [images, setImages] = useState<Array<IImageItem>>([]);
 
   const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
     // event.preventDefault();
     // const target = event.target;
 
-    const newImageItem: ImageItem = {
+    const newImageItem: IImageItem = {
       id: generateId(),
-      url: `https://randomfox.ca/images/${random()}.jpg`,
+      url: `https://randomfox.ca/images/${MyRandom()}.jpg`,
     };
 
     setImages([...images, newImageItem]);
+
+    // window.plausible("add_fox", {
+    //   lodash/index.d.ts
+    // })
   };
 
   return (
     <main>
       <h1>Hello Platzi</h1>
       <button onClick={addNewFox}>Add New Fox</button>
-      {images.map((image) => (
-        <div key={image.id} className="p-4">
+      {images.map(({ id, url }, index) => (
+        <div key={id} className="p-4">
           <LazyImage
-            src={image.url}
+            src={url}
             alt="fox"
             width={320}
             height="auto"
             title="Random Fox"
             className="rounded-md bg-neutral-100"
             onClick={() => console.log("hey")}
+            onLazyLoad={(img) => {
+              console.log(`Image #${index + 1} cargada. Nodo:`, img);
+            }}
           />
         </div>
       ))}
